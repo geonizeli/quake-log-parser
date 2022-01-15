40659d4818812aeb2cd03087d62f69dddd3cbee6
João Victor Geonizeli <joao.geonizeli@gmail.com>
2022-01-15 16:42:00 -0300

    koa setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+import Koa from 'koa';
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  ctx.body = `${ctx.method} ${ctx.url}`;
+});
+
+const DEFAULT_PORT = 3000;
+
+try {
+  app.listen(DEFAULT_PORT);
+  console.log(`Server is running on port ${DEFAULT_PORT}`);
+} catch (e) {
+  console.log(e);
+}
